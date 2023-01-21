9db2df213ed9d6b0324a0fd94b95ee0090093642
sodynet <81561199+sodynet@users.noreply.github.com>
2023-01-21 20:43:52 +0800

    Create simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,9 +2,9 @@
 # This script calculates simple interest given principal, annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: SETH (IBM)
 # Addtional Authors:
-# <your Github username>
+# <sodynet>
 
 # Input:
 # p, principal amount
